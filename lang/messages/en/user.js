9e0f48ae446686d129eed19c54ea15e9d8af62df
Anil <anilbronson12@gmail.com>
2025-09-25 16:27:38 -0700

    partB

---
+++ lang/messages/en/user.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    MESSAGE: "<p style = 'color: blue;'><b>Hello %1, What a beautiful day. Server current date and time is %2</b></p>"
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const dt = require('./modules/utils');
+const messages = require('./lang/messages/en/user');
+const url = require('url');
+
+http.createServer((req, res) => {
+    const q = url.parse(req.url, true).query;
+    const name = q.name || 'Error';
+    const message = messages.MESSAGE.replace('%1', name).replace('%2', dt.getDate());
+    res.writeHead(200, {'Content-Type': 
+    'text/html'});
+    res.write(`${message}`);
+    res.end();
+}).listen(8000);
